5010fee34301be905c56c533f61213165b8ab190
ngockhanhtran <trankhanh01092006@gmail.com>
2026-09-12 11:05:27 +0700

    Add project files.

---
+++ ViewComponents/HotProductViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN4.ViewComponents
+{
+    // Kế thừa từ ViewComponent
+    public class HotProductViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var hotProducts = new List<string>
+            {
+                "Nồi cơm điện cao tần Nagakawa NAG0102",
+                "Nồi cơm điện cao tần Nagakawa NAG0102",
+                "Nồi cơm điện cao tần Nagakawa NAG0102"
+            };
+
+            return await Task.FromResult(View(hotProducts));
+        }
+    }
+}
